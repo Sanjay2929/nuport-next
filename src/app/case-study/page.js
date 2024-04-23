890a0b1ccbd50089ab6a3e2335a999ae58e8dc17
radialcoderavipoonia <ravipoonia28122002@gmail.com>
2024-04-23 11:42:33 +0530

    image folder made

--- src/app/case-study/page.js
+++ src/app/case-study/page.js
@@ -1,12 +1,14 @@
 import CareerWithUs from "@/components/careers/CareerWithUs";
 import OpenJobs from "@/components/careers/OpenJobs";
 import Hero from "@/components/case-study-page/Hero";
+import Profile from "@/components/case-study-page/Profile";
 import React from "react";
 
 const page = () => {
   return (
     <>
       <Hero />
+      <Profile />
     </>
   );
 };
---
+++ src/components/case-study-page/Profile.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="bg-white">
+      <div className="py-20 xl:max-w-[1133px] container mx-auto px-5">
+        <p className="text-center font-plus font-medium text-[36px] leading-[140%] tracking-[0.72px]">
+          “Nuport is indeed an incredible platform for turning the whole process
+          of supply chain businesses into automation. This platform has made a
+          significant change in our supply chain by automating agricultural
+          distribution planning.”
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
