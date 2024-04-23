561a3638fe19d5abf578acd4abce178fc370629c
Sanjay2929 <Sanjayjyani220@gmail.com>
2024-04-23 10:41:01 +0530

    code merge

--- src/components/blogs/Revolution.jsx
+++ src/components/blogs/Revolution.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Revolution = ({ date, content, btn, img }) => {
   return (
-    <div className=" border-2 border-tealBlue rounded-xl bg-lightTeal mb-[24px] overflow-hidden min-h-[287px] flex transition-all ease-in-out duration-300 shadow-lg hover:shadow-tealBlue ">
+    <div className=" border-2 hover:border-tealBlue border-darkGray rounded-xl bg-lightTeal mb-[24px] overflow-hidden min-h-[287px] flex transition-all ease-in-out duration-300 ">
       <div className="flex flex-wrap">
         <div className=" sm:w-7/12 w-full sm:py-[26px] sm:px-[19px] p-5 flex flex-col justify-between items-start">
           <div className=" mb-5">
@@ -14,9 +14,9 @@ const Revolution = ({ date, content, btn, img }) => {
               {content}
             </p>
           </div>
-          <button className=" rounded-lg py-2 px-3 bg-darkGray text-offWhite font-plus font-semibold text-sm common_btn">
+          <span className=" rounded-lg py-2 px-3 bg-darkGray text-offWhite font-plus font-semibold text-sm">
             {btn}
-          </button>
+          </span>
         </div>
         <div className=" sm:w-5/12 w-full">
           <Image
