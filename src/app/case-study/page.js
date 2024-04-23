12eb08d4c5928fb1e5972199b54d95811129deea
sukhvinder_jaat <122784772+sukhvinder-jaat@users.noreply.github.com>
2024-04-23 11:16:46 +0530

    case study page start

---
+++ src/app/case-study/page.js
@@ -0,0 +1,14 @@
+import CareerWithUs from "@/components/careers/CareerWithUs";
+import OpenJobs from "@/components/careers/OpenJobs";
+import Hero from "@/components/casestudypage/Hero";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+};
+
+export default page;
