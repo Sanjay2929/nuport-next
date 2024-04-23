c8294eb0ef6eb3c4ee32edb4c64fc917d14efe42
sukhvinder_jaat <122784772+sukhvinder-jaat@users.noreply.github.com>
2024-04-23 11:43:38 +0530

    code merge

--- src/app/case-study/page.js
+++ src/app/case-study/page.js
@@ -1,12 +1,16 @@
 import CareerWithUs from "@/components/careers/CareerWithUs";
 import OpenJobs from "@/components/careers/OpenJobs";
 import Hero from "@/components/case-study-page/Hero";
+import Introduction from "@/components/case-study-page/Introduction";
 import React from "react";
 
 const page = () => {
   return (
     <>
       <Hero />
+      <div className="bg-darkBlue pt-[73px] pb-[143px]">
+        <Introduction />
+      </div>
     </>
   );
 };
---
+++ src/components/case-study-page/Introduction.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Introduction = () => {
+  return (
+    <div className="container 2xl:max-w-[1285px] px-5 2xl:px-0 mx-auto relative">
+      <div className=" flex flex-wrap">
+              <div className=" w-10/12 pe-3">
+                  <p className=" "></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Introduction;
