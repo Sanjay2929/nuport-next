905b6beb68583e6404ad3a88ef24a89e69531d52
radialcoderavipoonia <ravipoonia28122002@gmail.com>
2024-04-24 14:37:50 +0530

    cards done

--- src/components/pricing-e-commerce/Cards.jsx
+++ src/components/pricing-e-commerce/Cards.jsx
@@ -6,28 +6,33 @@ const Cards = () => {
     <div className="bg-darkBlue">
       <div className="xl:max-w-[1324px] px-5 mx-auto container">
         <div className="pb-40">
-          <div className="grid xl:grid-cols-3 grid-cols-2 gap-6">
+          <div className="flex justify-center gap-6 flex-wrap ">
             {eCommerce.map((value, index) => (
               <div
                 key={index}
-                className="p-[1.5px] bg-darkBlue hover:bg-[linear-gradient(180deg,_#288EA5_0%,_rgba(40,_142,_165,_0)_73.92%)]  rounded-[20px] transition-all ease-in-out duration-300 group"
+                className="p-[1.5px] sm:w-[calc(50%-12px)] xl:w-[calc(33.3%-16px)] bg-darkBlue hover:bg-[linear-gradient(180deg,_#288EA5_0%,_rgba(40,_142,_165,_0)_73.92%)]  rounded-[20px] transition-all ease-in-out duration-300 group max-w-[412px] sm:max-w-[unset] w-full mx-auto"
               >
-                <div className="h-[652px]  bg-darkBlue rounded-[20px] border border-darkGray p-7 relative overflow-hidden">
-                  <div className="w-full max-h-[300px] h-full blur-[190px] bg-transparent group-hover:bg-moonstone transition-all ease-in-out duration-300  absolute end-0 top-0 rounded-full"></div>
-                  <div className="w-[95px] h-[80px] relative z-10">
-                    {value.icon}
+                <div className="lg:h-[652px] h-[582px] bg-darkBlue group-hover:border-transparent rounded-[20px] border border-darkGray md:p-7 p-4 relative overflow-hidden ">
+                  <div className="w-[50%] max-h-[200px] h-full blur-[190px] bg-transparent group-hover:bg-moonstone transition-all ease-in-out duration-300  absolute end-0 top-0 rounded-full"></div>
+                  <div className="flex justify-between">
+                    <div className="w-[95px] h-[80px] relative z-10">
+                      {value.icon}
+                    </div>
+                    <button className={`bg-white px-3 h-[34px] text-shark rounded-lg font-plus font-semibold text-sm leading-[140%] ${index === 1 ? "block":"hidden"}`}>Most Popular</button>
                   </div>
-                  <p className="pt-9 text-white font-plus font-semibold text-2xl leading-[140%] relative z-10">
+                  <p className="lg:pt-9 pt-6 text-white font-plus font-semibold lg:text-2xl text-xl leading-[140%] relative z-10">
                     {value.title}
                   </p>
-                  <p className="pt-2 text-lightGray font-plus font-medium text-lg leading-[140%] relative z-10">
+                  <p className="pt-2 text-lightGray font-plus font-medium lg:text-lg text-base leading-[140%] relative z-10">
                     {value.description}
                   </p>
-                  <p className="pt-11 text-white font-plus text-3xl font-bold leading-[124%] ps-2 relative z-10">
+                  <p className="lg:pt-11 pt-7 text-white font-plus lg:text-3xl text-2xl font-bold leading-[124%] md:ps-2 relative z-10">
                     {value.months}{" "}
-                    <span className="font-medium text-2xl">/month</span>
+                    <span className="font-medium lg:text-2xl text-xl">
+                      /month
+                    </span>
                   </p>
-                  <button className="mt-7 mb-3 bg-darkGray w-full flex justify-center items-center h-[52px] rounded-[100px] text-white font-plus font-bold text-base leading-[140%] group-hover:bg-tealBlue transition-all duration-300 ease-in-out relative z-10">
+                  <button className="lg:mt-7 mt-5 mb-3 bg-darkGray w-full flex justify-center items-center h-[52px] rounded-[100px] text-white font-plus font-bold text-base leading-[140%] group-hover:bg-tealBlue transition-all duration-300 ease-in-out relative z-10">
                     {value.contact}
                   </button>
                   {value.features.map((subvalue, index) => {
@@ -36,7 +41,7 @@ const Cards = () => {
                         <span>
                           <PricingRightClick />
                         </span>
-                        <p className="text-white ps-2 font-plus font-medium text-lg leading-[140%]">
+                        <p className="text-white ps-2 font-plus font-medium lg:text-lg text-base leading-[140%]">
                           {subvalue}
                         </p>
                       </div>
